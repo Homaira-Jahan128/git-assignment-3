fea4d92950afbb58687cc75d853e6042b738520a
Homaira-Jahan128 <homaira.jahan128@gmail.com>
2025-03-11 12:16:22 +0600

    Used constants for menu readability

--- taskList.java
+++ taskList.java
@@ -1,31 +1,36 @@
 import java.util.*;
 
-class taskList {
+class TaskManager {
     static Scanner scanner = new Scanner(System.in);
-    static List<String> l = new ArrayList<>();
+    static List<String> taskList = new ArrayList<>();
+
+    private static final int ADD_ITEM = 1;
+    private static final int REMOVE_ITEM = 2;
+    private static final int SHOW_ITEMS = 3;
+    private static final int EXIT = 4;
 
     public static void main(String[] args) {
         while (true) {
             System.out.println("1: Add 2: Remove 3: Show 4: Exit");
             int choice = scanner.nextInt();
-            if (choice == 1) {
+
+            if (choice == ADD_ITEM) {
                 System.out.print("N: ");
                 scanner.nextLine();
-                l.add(scanner.nextLine());
-            }
-            else if (choice == 2) {
+                taskList.add(scanner.nextLine());
+            } else if (choice == REMOVE_ITEM) {
                 System.out.print("I: ");
                 int index = scanner.nextInt();
-                if (index >= 0 && index < l.size())
-                    l.remove(index);
-            }
-            else if (choice == 3) {
-                for (int position = 0; position < l.size(); position++) {
-                    System.out.println(position + ": " + l.get(position));
+                if (index >= 0 && index < taskList.size()) {
+                    taskList.remove(index);
                 }
-            }
-            else if (choice == 4)
+            } else if (choice == SHOW_ITEMS) {
+                for (int position = 0; position < taskList.size(); position++) {
+                    System.out.println(position + ": " + taskList.get(position));
+                }
+            } else if (choice == EXIT) {
                 break;
+            }
         }
     }
 }
